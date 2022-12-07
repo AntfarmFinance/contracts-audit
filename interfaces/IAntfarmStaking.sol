ec1fdadad084c3495829740bd711b58f33cf7b03
barbat0x <florian.chardel@gmail.com>
2022-12-07 17:19:30 +0100

    Delete interfaces directory

--- interfaces/IAntfarmStaking.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-interface IAntfarmStaking {
-    function depositVested(address _for, uint256 _amount) external;
-}
--- interfaces/IFlashloaner.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-interface IFlashloaner {
-    function flashloan(uint256 amount, bytes calldata data) external;
-
-    function wFlashloan(uint256 wAmount, bytes calldata data) external;
-}
